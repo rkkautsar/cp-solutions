e446af9812554360aa49543f6c603b88b672cc88
rkkautsar <rkkautsar@gmail.com>
2014-10-31 12:49:50 +0700

    Add UVa 11889

---
+++ UVa/11889.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// typedef
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<double,double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<bool> vb;
+ 
+//defines
+#define abs(_x) ((_x)<0?-(_x):(_x))
+#define REP(_x) for(int __x=0;__x<_x;++__x)
+#define mp(_x,_y) make_pair((_x),(_y))
+#define PI 3.1415926535897932385
+#define EPS 1e-9
+#define INF 0x7FFFFFFF
+#define INFLL 0x7FFFFFFFFFFFFFFFLL
+#define ceildiv(_a,_b) ((_a)%(_b)==0?(_a)/(_b):((_a)/(_b))+1)
+
+int main(int argc, char **argv){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);	
+	
+	int t,a,c,x,y;
+	cin>>t;
+	while(t--){
+		cin>>a>>c;
+		if(c<a || c%a!=0){
+			cout<<"NO SOLUTION\n";
+			continue;
+		}
+		x=a;
+		while(__gcd(x,c/a)!=1) x/=__gcd(x,c/a);
+		cout<<c/x<<'\n';
+	}
+
+	return 0;
+}
