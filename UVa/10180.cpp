56c7339acd500fa58c96db456d50361792f98511
rkkautsar <rkkautsar@gmail.com>
2014-11-29 17:57:01 +0700

    Added UVa 10108

---
+++ UVa/10180.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// typedef
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<double,double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<bool> vb;
+ 
+//defines
+#define abs(_x) ((_x)<0?-(_x):(_x))
+#define REP(_x) for(int __x=0;__x<_x;++__x)
+#define mp(_x,_y) make_pair((_x),(_y))
+#define PI acos(-1)
+#define EPS 1e-9
+#define INF 0x7FFFFFFF
+#define INFLL 0x7FFFFFFFFFFFFFFFLL
+#define ceildiv(_a,_b) ((_a)%(_b)==0?(_a)/(_b):((_a)/(_b))+1)
+
+struct point{ 
+    double x,y; 
+    point(){}
+    point(double a,double b):x(a),y(b){}
+};
+struct vec{ 
+    double x,y; 
+    vec(){}
+    vec(double dx,double dy):x(dx),y(dy){}
+    vec(point a,point b):x(b.x-a.x),y(b.y-a.y){}
+};
+struct line{
+	point a,b;
+	line(){}
+	line(point x,point y):a(x),b(y) {}
+};
+point translate(point p,vec v){
+    return point(p.x+v.x,p.y+v.y);
+}
+vec scale(vec v,double s){
+    return vec(v.x*s,v.y*s);
+}
+double dist(point a,point b){
+    return hypot(fabs(a.x-b.x),fabs(a.y-b.y));
+}
+double cross(vec a,vec b){
+    return (a.x*b.y)-(a.y*b.x);
+}
+double cross(point a,point b,point c){
+    return (b.x-a.x)*(c.y-a.y)-(b.y-a.y)*(c.x-a.x);
+}
+bool ccw(point a,point b,point c){
+    return cross(a,b,c)< -EPS;
+}
+bool collinear(point a,point b,point c){
+    return fabs(cross(a,b,c))<EPS;
+}
+bool parallel(point a,point b,point c,point d){
+    return fabs((fabs(a.y-b.y)/fabs(a.x-b.x))-(fabs(c.y-d.y)/fabs(c.x-d.x)))<EPS;
+}
+double dot(vec a,vec b){
+    return a.x*b.x+a.y*b.y;
+}
+double norm_sq(vec a){
+    return a.x*a.x+a.y*a.y;
+}
+bool is_intersect(point a,point b,point c,point d){
+    return (ccw(a,b,c)!=ccw(a,b,d)) && (ccw(c,d,a)!=ccw(c,d,b));
+}
+bool is_intersect(line a,line b){
+	return is_intersect(a.a,a.b,b.a,b.b);
+}
+bool in_segment(point p,point a,point b){
+    return fabs(dist(a,p)+dist(p,b)-dist(a,b))<EPS;
+}
+double dist_point_line(point p,point a,point b,point &intersection){
+    vec ap(a,p), ab(a,b);
+    double u=dot(ap,ab)/norm_sq(ab);
+    intersection=translate(a,scale(ab,u));
+    return dist(p,intersection);
+}
+point intersection(point a,point b,point c,point d){
+    point intersect;
+    double pembagi=(a.x-b.x)*(c.y-d.y)-(a.y-b.y)*(c.x-d.x);
+    intersect.x=(((a.x*b.y)-(a.y*b.x))*(c.x-d.x)-
+                 ((c.x*d.y)-(c.y*d.x))*(a.x-b.x))/pembagi;
+    intersect.y=(((a.x*b.y)-(a.y*b.x))*(c.y-d.y)-
+                 ((c.x*d.y)-(c.y*d.x))*(a.y-b.y))/pembagi;
+    return intersect;
+}
+point intersection(line a,line b){
+	return intersection(a.a,a.b,b.a,b.b);
+}
+double pythagoras(double a, double c){
+	return sqrt(c*c - a*a);
+}
+double angle(point A,point O,point B){
+	//returns minimum angle <AOB in radian
+	double hi,lo,ret;
+	hi=atan2(A.y-O.y,A.x-O.x);
+	lo=atan2(B.y-O.y,B.x-O.x);
+	if(lo>hi)swap(hi,lo);
+	ret=hi-lo;
+	if(ret<-EPS) ret=2*PI+ret;
+	if(ret>PI)ret=2*PI-ret;	
+	return ret;
+}
+
+int main(int argc, char **argv){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);	
+	
+	int t;
+	cin>>t;
+	point A,O(0,0),B,dummy;
+	double R,AO,OB,length_a,length_b,arc;
+	while(t--){
+		cin>>A.x>>A.y>>B.x>>B.y>>R;
+		
+		if((dist_point_line(O,A,B,dummy)+EPS>R && in_segment(dummy,A,B)) || !in_segment(dummy,A,B)){
+			//not intersect
+			cout<<fixed<<setprecision(3)<<dist(A,B)<<'\n';
+			continue;
+		}
+
+		AO=dist(A,O);
+		OB=dist(O,B);
+		length_a=pythagoras(R,AO);
+		length_b=pythagoras(R,OB);
+		arc=R*(angle(A,O,B)-(fabs(length_a)<EPS?0:asin(length_a/AO))-(fabs(length_b)<EPS?0:asin(length_b/OB)));
+
+		cout<<fixed<<setprecision(3)<<length_a+length_b+arc<<'\n';
+	}
+
+	return 0;
+}
